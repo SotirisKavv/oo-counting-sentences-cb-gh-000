b7075dd8f218d602d59bc5699e63ce1ec1f682e3
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-06 21:43:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,19 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    return self.end_with?(".");
+    return self.end_with?(".")
   end
 
   def question?
-    return self.end_with?("?");
+    return self.end_with?("?")
   end
 
   def exclamation?
-    return self.end_with?("!");
+    return self.end_with?("!")
   end
 
-  binding.pry
   def count_sentences
-    self.split.count
+    count = 0
+    i = 0
+
+    while i < self.split.count
+      if self.split[i] != nil
+        count += 1
+      end
+      i += 1
+    end
+    return count      
   end
 end
