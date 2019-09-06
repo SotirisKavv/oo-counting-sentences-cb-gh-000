d5ad5f60eb5c3189e25872ed8aa34fca3e9353c5
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-06 21:35:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".");
   end
 
   def question?
-
+    return self.end_with?("?");
   end
 
   def exclamation?
-
+    return self.end_with?("!");
   end
 
+  binding.pry
   def count_sentences
-
+    self.split.count
   end
 end
